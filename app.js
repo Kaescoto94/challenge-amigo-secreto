8dab5e85d4b3d2b9aa42ab46d93e0c53bfb69023
Kennet Escoto <kaescoto94@gmail.com>
2025-04-17 13:59:13 -0600

    Implementación de la función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 let amigos = [];
+let input = document.querySelector('#amigo');
+
+function agregarAmigo() {
+    //Capturar el valor del campo de entrada
+    let nombre = input.value.trim();
+
+    //Validar la entrada
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+
+    //Actualizar el array de amigos
+    amigos.push(nombre);
+
+    //Limpiar el campo de entrada
+    input.value = '';
+    console.log(amigos);
+    return;
+}
